50d4f57b11600fcff0a8acdd970dda5dc705cd4d
Olga F. Andreiko <olgafesh@outlook.com>
2021-04-09 12:40:35 +0200

    update jest lint

--- client/src/jest.lint.js
+++ client/src/jest.lint.js
@@ -2,5 +2,5 @@ module.exports = {
   displayName: "lint",
   runner: "jest-runner-eslint",
   testMatch: ["<rootDir>/**/*.js", "<rootDir>/**/*.ts", "<rootDir>/**/*.tsx"],
-  testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]", "<rootDir>/next.config.js","<rootDir>/cypress" ],
+  testPathIgnorePatterns: ["<rootDir>/next.config.js","<rootDir>/cypress" ],
 };
