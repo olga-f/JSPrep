6e94090bad505a4cbb24f3cbe7be267a79d9c4a3
Olga F. Andreiko <olgafesh@outlook.com>
2021-04-10 22:09:54 +0200

    clean header

--- client/src/lib/components/Header.tsx
+++ client/src/lib/components/Header.tsx
@@ -1,8 +1,5 @@
 import Link from "next/link";
-import Image from "next/image";
-
 import { GithubLogo } from "./github-logo";
-
 import { Button, KIND, SIZE, SHAPE } from "baseui/button";
 import { useStyletron } from "baseui";
 
@@ -17,7 +14,7 @@ export default function Header() {
         ...theme.typography.ParagraphMedium,
         display: "flex",
         flexWrap: "wrap",
-        paddingTop: theme.sizing.scale300,
+        paddingTop: theme.sizing.scale400,
         paddingBottom: theme.sizing.scale400,
         paddingLeft: theme.sizing.scale1000,
         paddingRight: theme.sizing.scale1000,
@@ -27,11 +24,7 @@ export default function Header() {
     >
       <div
         className={css({
-          //  marginLeft: "none",
           marginRight: "auto",
-          // display: "flex",
-          // alignItems: "center",
-          // order: 1,
         })}
       >
         <Link href="/">
@@ -42,30 +35,19 @@ export default function Header() {
           >
             <div
               className={css({
-                //  position: "absolute",
-                //  top: "50%",
-                //  left: "50%",
-                //   transform: "translate(-50%, -50%)",
-                padding:  theme.sizing.scale100,
+                padding: theme.sizing.scale100,
                 color: theme.colors.contentPrimary,
-                border: "3px solid black",
-                // width: 237,
-                //  height: 217,
-                // padding: 15,
+                border: `${theme.sizing.scale100} solid ${theme.colors.contentPrimary}`,
               })}
             >
               <h1
                 className={css({
                   fontSize: theme.sizing.scale700,
-                  // fontWeight: 100,
                   margin: "0",
                   textAlign: "right",
                   textTransform: "uppercase",
                   lineHeight: 0.9,
-                  letterSpacing: theme.sizing.scale100,
-
-                  //  transition: "all 250ms linear",
-                  //  fontVariationSettings: "wght 50, wdth 500",
+                  letterSpacing: theme.sizing.scale200,
                 })}
               >
                 JS
