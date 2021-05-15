c5b177a1b3cb31015a678cd809833007be025cda
Olga F. Andreiko <olgafesh@outlook.com>
2021-05-15 20:26:25 +0200

    add allowed hosts

--- server/jsprep/settings.py
+++ server/jsprep/settings.py
@@ -9,7 +9,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    config('URL_SERVER')
+]
 
 
 # Application definition
