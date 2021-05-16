9d24bf223fd6593b71b7da8fd3f6b4274997791b
Olga F. Andreiko <olgafesh@outlook.com>
2021-05-16 23:52:02 +0200

    add static settings

--- server/jsprep/settings.py
+++ server/jsprep/settings.py
@@ -113,7 +113,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'    
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 connect(host=config('_MONGODB_URI'))
 
