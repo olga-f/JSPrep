c5eb000644b6ab274d73d156cbbd5b5181865b50
Olga F. Andreiko <olgafesh@outlook.com>
2021-05-20 19:14:22 +0200

    add markdown to exercise content

--- client/sections/Exercise/components/Tutorial/index.tsx
+++ client/sections/Exercise/components/Tutorial/index.tsx
@@ -1,15 +1,27 @@
+import { useStyletron } from 'baseui';
 import { Cell, Grid } from 'baseui/layout-grid';
-import { H1, Paragraph1 } from 'baseui/typography';
+import { H1 } from 'baseui/typography';
+import Markdown from 'react-markdown';
 
 import { ExerciseProps } from '../../../../lib/types';
 
 export const Tutorial = ({ exercise }: ExerciseProps): JSX.Element => {
+  const [css, theme] = useStyletron();
   return (
     <Grid>
       <Cell span={[0, 1, 2]}></Cell>
       <Cell span={[4, 6, 8]}>
         <H1>{exercise.name}</H1>
-        <Paragraph1>{exercise.content}</Paragraph1>
+        <span className="markdown">
+          <Markdown
+            className={css({
+              ...theme.typography.ParagraphMedium,
+              marginBottom: theme.sizing.scale900,
+            })}
+          >
+            {exercise.content ?? ""}
+          </Markdown>
+        </span>
       </Cell>
       <Cell span={[0, 1, 2]}></Cell>
     </Grid>
